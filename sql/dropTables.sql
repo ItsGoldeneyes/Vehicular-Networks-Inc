995d1b7e625d3553f36f9a9bbfe81494394d390c
Stanley Chow <51177611+Stanley-C-02@users.noreply.github.com>
2024-11-12 17:32:08 -0500

    add sql scripts for postgresql

---
+++ sql/dropTables.sql
@@ -0,0 +1,6 @@
+DROP TABLE public.poll_res;
+DROP TABLE public.poll;
+DROP TABLE public.survey_res;
+DROP TABLE public.survey;
+DROP TABLE public.feedback;
+DROP TABLE public.profile;
---
+++ sql/enums.sql
@@ -0,0 +1,1 @@
+CREATE TYPE profile_status AS ENUM ('normal', 'admin');
