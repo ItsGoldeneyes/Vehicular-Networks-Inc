940ea2320532624ae55776353caad0b5a6a31700
Stanley Chow <51177611+Stanley-C-02@users.noreply.github.com>
2024-11-12 18:01:36 -0500

    add example code to connect to db

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,37 @@
 from flask import Flask, jsonify
+import psycopg2
 import os
 
 app = Flask(__name__)
 
+def get_db_connection():
+    conn = psycopg2.connect('postgresql://postgres:WbVwEVfMeYzfzUGnrUKzmRfdHGRHjOHS@autorack.proxy.rlwy.net:31200/railway')
+    # conn = psycopg2.connect(host='autorack.proxy.rlwy.net',
+    #                         database='railway',
+    #                         user=os.environ['DB_USERNAME'],
+    #                         password=os.environ['DB_PASSWORD'])
+    return conn
+
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    response = jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
+@app.route('/test-db')
+def testdb():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM public.profile;')
+    data = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    response = jsonify(data)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 
 if __name__ == '__main__':
