b607757b90c59f27a278ad4e5ae1fad17571d99d
just2d2 <justin.rodrigues@ryerson.ca>
2024-11-14 12:29:43 -0500

    Polls Theme Color Fix

--- frontend/src/components/Polls/Polls.js
+++ frontend/src/components/Polls/Polls.js
@@ -1,12 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useOutletContext } from "react-router-dom";
-import {
-  Box,
-  Typography,
-  TextField,
-  Button,
-  Rating,
-} from "@mui/material";
+import { Box, Typography, TextField, Button, Rating } from "@mui/material";
 
 export default function Polls() {
   const { theme } = useOutletContext(); // Get theme from context
@@ -18,8 +12,9 @@ export default function Polls() {
   const fetchPollData = async () => {
     const data = {
       poll_name: "How are we Doing?",
-      poll_text: "Let us know how satisfied you are with recent exclusive offers.",
-      poll_label_l: "Bad", // dont think we need this 
+      poll_text:
+        "Let us know how satisfied you are with recent exclusive offers.",
+      poll_label_l: "Bad", // dont think we need this
       poll_label_r: "Great", // dont think we need this
       poll_min: 1,
       poll_max: 5,
@@ -36,69 +31,106 @@ export default function Polls() {
     console.log("Submitted Comment:", comment);
     // api call here to post to poll res
   };
-//theme declaration also not needed?
+  //theme declaration also not needed?
   return (
-    <div className={`polls ${theme}`}> 
-    <Box
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      justifyContent="center"
-    >
-          <Typography variant="h4" color="primary" gutterBottom>
-            {pollData.poll_name}
-          </Typography>
+    <div className={`polls ${theme}`}>
       <Box
-        sx={{
-          maxWidth: 400,
-          padding: 3,
-          border: "1px solid #ccc",
-          borderRadius: 2,
-          boxShadow: 3,
-          backgroundColor: "#fff",
-        }}
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
       >
-        <Typography variant="body1" sx={{ marginBottom: 2 }}>
-          {pollData.poll_text}
-        </Typography>
-        <Typography variant="body2" sx={{ marginBottom: 2 }}>
-          You will receive XXX points on submission.
-        </Typography>
-
-        <Box display="flex" alignItems="center" justifyContent="center">
-          {/* <Typography variant="body1">{pollData.poll_label_l}</Typography> */}
-          <Rating
-            name="poll-rating"
-            value={rating}
-            onChange={(e, newValue) => {
-              setRating(newValue);
-            }}
-            size="large"
-            max={pollData.poll_max}
-          />
-          {/* <Typography variant="body1">{pollData.poll_label_r}</Typography> */}
-        </Box>
-
-        <TextField
-          label="Any additional comments?" //picked up from wireframe. not stored in db, not needed
-          multiline
-          rows={4}
-          value={comment}
-          onChange={(e) => setComment(e.target.value)}
-          fullWidth
-          sx={{ marginTop: 2 }}
-        />
-
-        <Button
-          variant="contained"
-          fullWidth
-          sx={{ marginTop: 2 }}
-          onClick={handleSubmit}
+        <Typography
+          variant="h4"
+          gutterBottom
+          sx={{
+            color: theme === "dark" ? "white" : "black",
+          }}
         >
-          Submit
-        </Button>
+          {pollData.poll_name}
+        </Typography>
+        <Box
+          sx={{
+            maxWidth: 400,
+            padding: 3,
+            border: "1px solid #ccc",
+            borderRadius: 2,
+            boxShadow: 3,
+            backgroundColor: theme === "dark" ? "darkgray" : "white",
+          }}
+          slotProps={{
+            input: {
+              style: {
+                color: theme === "dark" ? "white" : "black",
+              },
+            },
+          }}
+        >
+          <Typography
+            variant="body1"
+            sx={{
+              marginBottom: 2,
+              color: theme === "dark" ? "white" : "black",
+            }}
+          >
+            {pollData.poll_text}
+          </Typography>
+          <Typography
+            variant="body2"
+            sx={{
+              marginBottom: 2,
+              color: theme === "dark" ? "white" : "black",
+            }}
+          >
+            You will receive XXX points on submission.
+          </Typography>
+
+          <Box display="flex" alignItems="center" justifyContent="center">
+            {/* <Typography variant="body1">{pollData.poll_label_l}</Typography> */}
+            <Rating
+              name="poll-rating"
+              value={rating}
+              onChange={(e, newValue) => {
+                setRating(newValue);
+              }}
+              size="large"
+              max={pollData.poll_max}
+            />
+            {/* <Typography variant="body1">{pollData.poll_label_r}</Typography> */}
+          </Box>
+
+          <TextField
+            label="Any additional comments?" //picked up from wireframe. not stored in db, not needed
+            multiline
+            rows={4}
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            fullWidth
+            sx={{ marginTop: 2 }}
+            slotProps={{
+              input: {
+                style: {
+                  color: theme === "dark" ? "white" : "black",
+                },
+              },
+              inputLabel: {
+                style: {
+                  color: theme === "dark" ? "white" : "black",
+                },
+              },
+            }}
+          />
+
+          <Button
+            variant="contained"
+            fullWidth
+            sx={{ marginTop: 2 }}
+            onClick={handleSubmit}
+          >
+            Submit
+          </Button>
+        </Box>
       </Box>
-    </Box>
     </div>
   );
 }
