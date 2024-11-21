7b48c10c52c4f216e9f595430de8b25fc3b95d44
Stanley Chow <51177611+Stanley-C-02@users.noreply.github.com>
2024-11-20 21:04:09 -0500

    fix constraints and types, add new test data

--- sql/allCommands.sql
+++ sql/allCommands.sql
@@ -12,7 +12,8 @@ create table public.form (
   name		varchar DEFAULT 'A form' NOT NULL,
   type		form_type NOT NULL,
   created_by	uuid NOT NULL,
-  created_at	timestamp DEFAULT now() NOT NULL
+  created_at	timestamp DEFAULT now() NOT NULL,
+  points		int2 DEFAULT 1 NOT NULL
 );
 create table public.form_question (
   form_id		uuid,
@@ -22,7 +23,8 @@ create table public.form_question (
   options		json,
 
   FOREIGN KEY (form_id) REFERENCES public.form (id),
-  PRIMARY KEY (form_id, question_num)
+  PRIMARY KEY (form_id, question_num),
+  CONSTRAINT has_options CHECK (type <> 'multiple_choice' OR options IS NOT NULL)
 );
 create table public.form_response (
   id				uuid PRIMARY KEY DEFAULT gen_random_uuid(),
@@ -31,18 +33,20 @@ create table public.form_response (
   user_id			uuid,
   freeform_answer	varchar,
   rate_answer		int2,
-  mc_answer		int2,
+  mc_answer		varchar,
 
   FOREIGN KEY (form_id) REFERENCES public.form (id),
   FOREIGN KEY (user_id) REFERENCES public.profile (user_id),
+  CONSTRAINT provided_answer CHECK (freeform_answer IS NOT NULL OR rate_answer IS NOT NULL OR mc_answer IS NOT NULL),
+  CONSTRAINT valid_rating CHECK (1 <= rate_answer AND rate_answer <= 5)
 );
 create table public.training_session (
-  id			uuid PRIMARY KEY,
-  title		varchar NOT NULL,
+  id			uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  title		varchar NOT NULL DEFAULT 'A Training Session',
   description	varchar,
-  points		int2
+  points		int2 NOT NULL DEFAULT 1
 );
-create table public.attendance(
+create table public.attendance (
   training_id	uuid,
   user_id		uuid,
 
@@ -51,36 +55,41 @@ create table public.attendance(
   PRIMARY KEY (training_id, user_id)
 );
 
-DROP TABLE public.attendance;
-DROP TABLE public.training_session;
-DROP TABLE public.form_response;
-DROP TABLE public.form_question;
-DROP TABLE public.form;
-DROP TABLE public.profile;
-
 -- Placeholder data
 
 INSERT INTO profile (user_id, username, email, password, profile_status) VALUES ('d45a0d11-acb0-43cc-b20b-e2ab8c1444e2', 'admin', 'admin@admin.com', 'admin', 'admin');
 INSERT INTO profile (user_id, username, email, password) VALUES ('189684c2-569a-436e-8d25-47cf07449d11', 'hamala@karris.party', 'Hamala Karris', 'admin');
 INSERT INTO profile (user_id, username, email, password) VALUES ('f18c9489-9dbd-4c28-ae5e-7af35e1dc308', 'tonald@drump.party', 'Tonald Drump', 'admin');
 
-CREATE TYPE profile_status AS ENUM ('normal', 'admin');
-CREATE TYPE form_type AS ENUM ('feedback', 'survey');
-CREATE TYPE question_type AS ENUM ('freeform', 'rate', 'multiple_choice');
-
-INSERT INTO form (id, name, type, created_by) VALUES ('20c1200f-cc03-4b80-9349-f19f6e826d03', 'Feedback Form', 'feedback', 'd45a0d11-acb0-43cc-b20b-e2ab8c1444e2');
+INSERT INTO form (id, name, type, created_by, points) VALUES ('20c1200f-cc03-4b80-9349-f19f6e826d03', 'Feedback Form', 'feedback', 'd45a0d11-acb0-43cc-b20b-e2ab8c1444e2', 10);
 INSERT INTO form_question (form_id, question_num, type, description) VALUES ('20c1200f-cc03-4b80-9349-f19f6e826d03', 1, 'freeform', 'Is there any sort of feedback you want to give us?');
 
-INSERT INTO form (id, name, type, created_by) VALUES ('a04c1d1b-f4fd-4dc7-ab39-86a83a3b70aa', 'A Poll', 'survey', 'd45a0d11-acb0-43cc-b20b-e2ab8c1444e2');
+INSERT INTO form_response (question_num, form_id, user_id, mc_answer) VALUES (1, '20c1200f-cc03-4b80-9349-f19f6e826d03', '189684c2-569a-436e-8d25-47cf07449d11', 'Great system! I think more frequent seasonal offers would be better.');
+
+INSERT INTO form (id, name, type, created_by, points) VALUES ('a04c1d1b-f4fd-4dc7-ab39-86a83a3b70aa', 'A Poll', 'survey', 'd45a0d11-acb0-43cc-b20b-e2ab8c1444e2', 20);
 INSERT INTO form_question (form_id, question_num, type, description) VALUES ('a04c1d1b-f4fd-4dc7-ab39-86a83a3b70aa', 1, 'rate', 'Let us know how satisfied you are with the recent exclusive FleetRewards offers.');
 INSERT INTO form_question (form_id, question_num, type, description) VALUES ('a04c1d1b-f4fd-4dc7-ab39-86a83a3b70aa', 2, 'freeform', 'Any additional comments?');
 
-INSERT INTO form (id, name, type, created_by) VALUES ('38c09435-975e-47c7-bb97-ab072c326764', 'A Survey', 'survey', 'd45a0d11-acb0-43cc-b20b-e2ab8c1444e2');
-INSERT INTO form_question (form_id, question_num, type, description) VALUES ('38c09435-975e-47c7-bb97-ab072c326764', 1, 'multiple_choice', 'Overall, Vehicular Services Inc. meets all your fleet''s needs.');
-INSERT INTO form_question (form_id, question_num, type, description) VALUES ('38c09435-975e-47c7-bb97-ab072c326764', 2, 'multiple_choice', 'Vehicular Services Inc.''s products are helpful to your fleet.');
-INSERT INTO form_question (form_id, question_num, type, description) VALUES ('38c09435-975e-47c7-bb97-ab072c326764', 3, 'multiple_choice', 'Vehicular Services Inc.''s services are helpful to your fleet.');
-INSERT INTO form_question (form_id, question_num, type, description) VALUES ('38c09435-975e-47c7-bb97-ab072c326764', 4, 'multiple_choice', 'Vehicular Services Inc.''s FleetRewards loyalty program is useful.');
+INSERT INTO form_response (question_num, form_id, user_id, mc_answer) VALUES (1, 'a04c1d1b-f4fd-4dc7-ab39-86a83a3b70aa', '189684c2-569a-436e-8d25-47cf07449d11', 5);
+INSERT INTO form_response (question_num, form_id, user_id, mc_answer) VALUES (1, 'a04c1d1b-f4fd-4dc7-ab39-86a83a3b70aa', '189684c2-569a-436e-8d25-47cf07449d11', 'None. Great job!');
+
+INSERT INTO form (id, name, type, created_by, points) VALUES ('38c09435-975e-47c7-bb97-ab072c326764', 'A Survey', 'survey', 'd45a0d11-acb0-43cc-b20b-e2ab8c1444e2', 50);
+INSERT INTO form_question (form_id, question_num, type, description, options) VALUES ('38c09435-975e-47c7-bb97-ab072c326764', 1, 'multiple_choice', 'Overall, Vehicular Services Inc. meets all your fleet''s needs.', '["Yes", "No"]');
+INSERT INTO form_question (form_id, question_num, type, description, options) VALUES ('38c09435-975e-47c7-bb97-ab072c326764', 2, 'multiple_choice', 'Vehicular Services Inc.''s products are helpful to your fleet.', '["Agree", "Neither", "Disagree"]');
+INSERT INTO form_question (form_id, question_num, type, description, options) VALUES ('38c09435-975e-47c7-bb97-ab072c326764', 3, 'multiple_choice', 'Vehicular Services Inc.''s services are helpful to your fleet.', '["True", "False"]');
+INSERT INTO form_question (form_id, question_num, type, description, options) VALUES ('38c09435-975e-47c7-bb97-ab072c326764', 4, 'multiple_choice', 'Vehicular Services Inc.''s FleetRewards loyalty program is useful.', '["Strongly Agree", "Agree", "Neither agree nor disagree", "Disagree", "Strongly Disagree"]');
 INSERT INTO form_question (form_id, question_num, type, description) VALUES ('38c09435-975e-47c7-bb97-ab072c326764', 5, 'freeform', 'Enter any additional comments you have for us (Vehicular Services Inc. or the FleetRewards program).');
+
+INSERT INTO form_response (question_num, form_id, user_id, mc_answer) VALUES (1, '38c09435-975e-47c7-bb97-ab072c326764', '189684c2-569a-436e-8d25-47cf07449d11', 'Yes');
+INSERT INTO form_response (question_num, form_id, user_id, mc_answer) VALUES (2, '38c09435-975e-47c7-bb97-ab072c326764', '189684c2-569a-436e-8d25-47cf07449d11', 'Neither');
+INSERT INTO form_response (question_num, form_id, user_id, mc_answer) VALUES (3, '38c09435-975e-47c7-bb97-ab072c326764', '189684c2-569a-436e-8d25-47cf07449d11', 'True');
+INSERT INTO form_response (question_num, form_id, user_id, mc_answer) VALUES (4, '38c09435-975e-47c7-bb97-ab072c326764', '189684c2-569a-436e-8d25-47cf07449d11', 'Strongly Agree');
+INSERT INTO form_response (question_num, form_id, user_id, freeform_answer) VALUES (5, '38c09435-975e-47c7-bb97-ab072c326764', '189684c2-569a-436e-8d25-47cf07449d11', 'N/A');
+
+INSERT INTO training_session (id, title, description, points) VALUES ('0a736891-01d6-4c00-afed-8cb32550f29d', 'Getting Started in VN Inc.', 'The first video to get you started!', 10);
+
+INSERT INTO attendance (training_id, user_id) VALUES ('0a736891-01d6-4c00-afed-8cb32550f29d', 'd45a0d11-acb0-43cc-b20b-e2ab8c1444e2');
+INSERT INTO attendance (training_id, user_id) VALUES ('0a736891-01d6-4c00-afed-8cb32550f29d', '189684c2-569a-436e-8d25-47cf07449d11');
 
 -- enums/types
 
@@ -91,3 +100,12 @@ CREATE TYPE question_type AS ENUM ('freeform', 'rate', 'multiple_choice');
 DROP TYPE profile_status;
 DROP TYPE form_type;
 DROP TYPE question_type;
+
+-- Drop Tables
+
+DROP TABLE public.attendance;
+DROP TABLE public.training_session;
+DROP TABLE public.form_response;
+DROP TABLE public.form_question;
+DROP TABLE public.form;
+DROP TABLE public.profile;
